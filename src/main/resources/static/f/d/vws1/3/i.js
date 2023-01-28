3ecb1a64dc5b841531cfe28c8d1b389269149963
romanm <roman.mishchenko@gmail.com>
2023-01-28 03:03:27 +0200

    d.pageVl.openedAdnVlMenu

--- src/main/resources/static/f/d/vws1/3/i.js
+++ src/main/resources/static/f/d/vws1/3/i.js
@@ -11,31 +11,37 @@ export default message
 const dataOed01 = jsLib1.makeEl(d, 'siteTitle count hw')
 dataOed01.tree = jsLib1.tree
 dataOed01.pageVl = d.pageVl
+dataOed01.eMap = d.eMap
 
 const oed01 = createApp({
     data() { return dataOed01 }, methods: {
-        test(lr) {
-            console.log(123, lr)
+        closeAdnVlMenu() {
+            d.pageVl.openedAdnVlMenu = null
+            this.count++
         },
     }
 })
 
 oed01.component('t-oed01-value', {
     data() { return d.eMap[this.adnId] }, methods: {
+        i(n) { return d.eMap[this.adnId] && d.eMap[this.adnId][n] }
     }, template: "#tOed01Value", props: { adnId: Number },
 })
-
 oed01.component('t-oed01-lmenu', {
-    data() { return d.eMap[this.adnId] }, methods: {
-        edAdnValue01() {
+    data() { return dataOed01 }, methods: {
+        i(n) { return d.eMap[this.adnId] && d.eMap[this.adnId][n] },
+        closeAdnVlMenu() {
+            d.pageVl.openedAdnVlMenu = null
+            this.count++
+        }, edAdnValue01() {
             console.log('tt01', this.adnId, this.parent, this.isOpen)
-            dataOed01.pageVl.subSepMenuName = 'name_' + this.adnId
+            d.pageVl.openedAdnVlMenu = this.adnId
+            this.count++
         }
     }, template: "#tOed01Lmenu", props: { adnId: Number },
 })
 
 oed01.mount('#oed01')
-
 
 createApp({ data() { return dataOed01 } }).mount('#headTitle')
 createApp({
@@ -43,16 +49,3 @@ createApp({
         rld() { this.count++ },
     }
 }).mount('#headPage')
-
-//- for test-
-
-const app1 = createApp({
-    data() { return dataOed01 }, methods: {
-        f02app1() {
-            this.count++
-            d.pageVl.y += 3
-            console.log(123, 'f02app1', this.count, d)
-        },
-    }
-}); app1.mount('#app1')
-
