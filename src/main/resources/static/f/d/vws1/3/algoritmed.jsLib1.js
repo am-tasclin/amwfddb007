8a65bf7a697788ea800aa7fb2df81386c20a2c5e
romanm <roman.mishchenko@gmail.com>
2023-01-29 14:05:41 +0200

    jsLib1.wsDbSelect

---
+++ src/main/resources/static/f/d/vws1/3/algoritmed.jsLib1.js
@@ -0,0 +1,115 @@
+/**
+ * @license Algoritmed.js v0.1.025
+ * (c) 2021-2023 Algoritmed Ltd. http://algoritmed.com
+ * License: Apache-2.0 license 
+ */
+
+'use strict'
+const jsLib1 = {}, sql_app = {}
+jsLib1.tree = { l: [], r: [] }
+jsLib1.hash = window.location.hash
+jsLib1.isHash = hn => jsLib1.hash.includes(hn + '_')
+jsLib1.init = () => jsLib1.isHash('init'
+) && JSON.parse(decodeURI(jsLib1.hash.split("init_")[1]))
+
+if (jsLib1.isHash('init')) {
+    jsLib1.tree.l = jsLib1.init().tree.l.id
+    jsLib1.tree.r = jsLib1.init().tree.r.id
+} else if (jsLib1.isHash('tree')) {
+    jsLib1.tree.l = jsLib1.hash.split('tree_')[1].split(',')[0].split('_')
+    jsLib1.tree.r = jsLib1.hash.split('tree_')[1].split(',')[1].split('_')
+}
+
+jsLib1.treeStr = () => jsLib1.tree.l.join('_') + ',' + jsLib1.tree.r.join('_')
+
+jsLib1.makeElFrom = (el, propsList) => {
+    const r = {}, nl = propsList.trim().split(' ')
+    el && nl.forEach(prop => el[prop] && (r[prop] = el[prop]))
+    return r
+}
+
+const d = {
+    count: 1,
+    // tree: jsLib1.tree,
+    siteTitle: 'Vue02WebSocket: (vws1/2) ',
+    eMap: {
+        "369864": {
+            "doc_id": 369864, "doctype": null, "reference": 369789
+            , "parent": 373473, "reference2": 369767, "r_doctype": null, "value_22": "ServiceRequest"
+            , "value_u_22": null, "value_24": null, "value_25": null, "rr_value_22": "Resource", "r_value_22": "DomainResource"
+            , "r2_doctype": null, "r2_value_22": "Request", "sort": 11
+        }
+        , "373530": {
+            "doc_id": 373530, "doctype": 14, "reference": null, "parent": 45, "reference2": null
+            , "r_doctype": null, "value_22": "eHealth in ua", "value_u_22": null, "value_24": null, "value_25": null
+            , "rr_value_22": null, "r_value_22": null, "r2_doctype": null, "r2_value_22": null, "sort": 1
+        }
+    }
+}, pageVl = {}
+d.pageVl = pageVl
+
+sql_app.adn01OneNode = {
+    name: "adn01OneNode: 'One Data Node' ::adn01",
+    sql: "SELECT * FROM (:sql_app.adn01 ) x \n\
+WHERE doc_id  = :adnId",
+}
+
+sql_app.adn01 = {
+    name: 'adn01: Abstract Data Node 01',
+    sql: "SELECT d.*, dr.doctype r_doctype, s.value value_22, su.value value_u_22, f.value value_24  \n\
+    , ts.value value_25, srr.value rr_value_22 \n\
+    , sr.value r_value_22, dr2.doctype r2_doctype \n\
+    , sr2.value r2_value_22, o.sort \n\
+    FROM doc d \n\
+    LEFT JOIN string s ON s.string_id =d.doc_id \n\
+    LEFT JOIN string_u su ON su.string_u_id =d.doc_id \n\
+    LEFT JOIN double f ON f.double_id =d.doc_id \n\
+    LEFT JOIN timestamp ts ON ts.timestamp_id =d.doc_id \n\
+     LEFT JOIN doc dr ON dr.doc_id =d.reference \n\
+     LEFT JOIN string sr ON sr.string_id =d.reference \n\
+     LEFT JOIN string srr ON srr.string_id =dr.reference \n\
+     LEFT JOIN doc dr2 ON dr2.doc_id =d.reference2 \n\
+     LEFT JOIN string sr2 ON sr2.string_id =d.reference2 \n\
+     LEFT JOIN sort o ON sort_id =d.doc_id ",
+}
+
+jsLib1.replaceSql = sql => {
+
+    while (sql.includes(':fn_sql_app.')) {
+        const sql_fnStr = sql.split(':fn_sql_app.')[1].split(' ')[0]
+        let sql_fnPath = sql_fnStr.split('.')
+        const fnName = firstFunctionName(sql_fnPath)
+        // console.log(sql_fnStr, fnName)
+        const fnParamsStr = sql_fnStr.split(fnName + '(')[1].split(')')[0]
+        // console.log(sql_fnStr, fnName, '\n-fn(PARAMS)->\n', fnParamsStr)
+        sql_fnPath = sql_fnStr.split('.')
+        // console.log(sql_fnStr, sql_fnPath, 1)
+        const fnObj = firstFunctionObj(sql_app[sql_fnPath.shift()], sql_fnPath)
+        sql_fnPath = fnParamsStr.split('.')
+        // console.log(sql_fnPath)
+        const fnParamObj = pathValue(sql_app[sql_fnPath.shift()], sql_fnPath)
+        // console.log(fnObj, fnParamObj)
+        let fnSql = fnObj(fnParamObj)
+        sql = sql.replace(':fn_sql_app.' + sql_fnStr, fnSql)
+        // console.log(sql)
+    }
+
+    while (sql.includes(':var_sql_app.')) {
+        const sql_varName = sql.split(':var_sql_app.')[1].split(' ')[0],
+            sql_varPath = sql_varName.split('.'),
+            sql_varVal = pathValue(sql_app[sql_varPath.shift()], sql_varPath)
+        // console.log(sql_varName, '=', sql_varVal)
+        sql = sql.replace(':var_sql_app.' + sql_varName, sql_varVal)
+    }
+
+    // let sql_app = ':sql_app'
+    let sql_app = ':sql_app.'
+    while (sql.includes(sql_app)) {
+        let sql_name = sql.split(sql_app)[1].split(' ')[0]
+        // console.log(sql_name)
+        let sql_inner = jsLib1.readSql2R(sql_name)
+        sql = sql.replace(sql_app + sql_name, sql_inner)
+    }
+    return '' + sql
+}
+jsLib1.readSql2R = sqlN => sql_app[sqlN] && jsLib1.replaceSql(sql_app[sqlN].sql)
--- src/main/resources/static/f/d/vws1/3/i.js
+++ src/main/resources/static/f/d/vws1/3/i.js
@@ -2,54 +2,88 @@
 const { createApp } = Vue
 
 const message = () => {
-    const name = "Jesse";
-    const age = 40;
-    return name + ' is ' + age + 'years old.';
+    const name = "Jesse"
+    const age = 40
+    return name + ' is ' + age + 'years old.'
 }
-export default message
+//export default message
 
+jsLib1.wsDbSelect = new WebSocket("ws://localhost:8007/dbSelect"
+); jsLib1.wsDbSelect.onopen = event => {
+    console.log(123)
+    readAdns(jsLib1.tree.r)
+}; jsLib1.wsDbSelect.onmessage = event => {
+    const obj = JSON.parse(event.data)
+    console.log(obj.adnId, obj.list[0])
+    if (!dataOed01.eMap[obj.adnId]) {
+        console.log(obj.adnId, obj)
+        dataOed01.eMap[obj.adnId] = obj.list[0]
+    }
+}
 
-const dataOed01 = jsLib1.makeEl(d, 'siteTitle count')
+const readAdns = a => a.forEach(adnId => {
+    if (!d.eMap[adnId]) {
+        const sql = jsLib1.replaceSql(sql_app.adn01OneNode.sql
+        ).replace(':adnId', adnId)
+            , jn = { adnId: adnId, sql: sql }
+        jsLib1.wsDbSelect.send(JSON.stringify(jn))
+    }
+})
+
+const dataOed01 = jsLib1.makeElFrom(d, 'siteTitle count')
 dataOed01.tree = jsLib1.tree
 dataOed01.pageVl = d.pageVl
 dataOed01.eMap = d.eMap
+dataOed01.leMap = Object.keys(dataOed01.eMap).length
+jsLib1.i = (adnId, n) => d.eMap[adnId] && d.eMap[adnId][n]
+export default dataOed01
 
 const oed01 = createApp({
     data() { return dataOed01 }, methods: {
         closeAdnVlMenu() {
             d.pageVl.openedAdnVlMenu = null
             this.count++
-        }, adnMO(adnId) {
-            console.log(adnId)
+        }, adnMO(adnId) {//MO:mouseover
             d.pageVl.adnIdMO = adnId
             this.count++
-        }
+        }, o(adnId) { return d.eMap[adnId] }
+        , i(n) { return jsLib1.i(this.adnId, n) }
     }
-})
-
-oed01.component('t-oed01-value', {
-    data() { return d.eMap[this.adnId] },
+}); oed01.component('t-oed01-value', {
+    methods: {
+        i(n) { return jsLib1.i(this.adnId, n) },
+    }, data() { return d.eMap[this.adnId] },
     template: "#tOed01Value", props: { adnId: Number },
-})
-oed01.component('t-oed01-lmenu', {
-    data() { return dataOed01 }, methods: {
-        i(n) { return d.eMap[this.adnId] && d.eMap[this.adnId][n] },
+}); oed01.component('t-oed01-mo', {
+    methods: {
+        i(n) { return jsLib1.i(this.adnId, n) },
+    }, template: "#tOed01Mo", props: { adnId: Number }, data() { return dataOed01 },
+}); oed01.component('t-oed01-lmenu', {
+    methods: {
         closeAdnVlMenu() {
             d.pageVl.openedAdnVlMenu = null
             this.count++
         }, edAdnValue01() {
-            console.log('tt01', this.adnId, this.parent, this.isOpen)
             d.pageVl.openedAdnVlMenu = this.adnId
             this.count++
-        }
-    }, template: "#tOed01Lmenu", props: { adnId: Number },
-})
-
-oed01.mount('#oed01')
+        }, i(n) { return jsLib1.i(this.adnId, n) },
+    }, template: "#tOed01Lmenu", props: { adnId: Number }, data() { return dataOed01 },
+}); oed01.mount('#oed01')
 
 createApp({ data() { return dataOed01 } }).mount('#headTitle')
+
+console.log(123)
+
 createApp({
-    data() { return dataOed01 }, methods: {
-        rld() { this.count++ },
-    }
+    methods: {
+        rld() {
+            // readAdns(jsLib1.tree.r)
+            console.log(d.eMap)
+            this.count++
+            if (Object.keys(dataOed01.eMap).length != dataOed01.leMap) {
+                console.log(Object.keys(dataOed01.eMap).length, dataOed01.leMap)
+            }
+        },
+        treeStr() { return jsLib1.treeStr() },
+    }, data() { return dataOed01 },
 }).mount('#headPage')
