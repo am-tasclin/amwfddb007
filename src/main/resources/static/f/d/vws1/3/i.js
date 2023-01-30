7eb5bba1695f1b82153c3dbf1dfe91d2ba704e7c
romanm <roman.mishchenko@gmail.com>
2023-01-31 01:11:53 +0200

    ORdeMS:       --     d_Oed01

--- src/main/resources/static/f/d/vws1/3/i.js
+++ src/main/resources/static/f/d/vws1/3/i.js
@@ -17,8 +17,8 @@ jsLib1.wsDbSelect.onmessage = event => {
     if ('adn01Childrens' == obj.sqlName
     ) fillInParentChild(obj)
     else if ('adn01OneNode' == obj.sqlName) {
-        if (!dataOed01.eMap[obj.adnId]) {
-            dataOed01.eMap[obj.adnId] = obj.list[0]
+        if (!d_Oed01.eMap[obj.adnId]) {
+            d_Oed01.eMap[obj.adnId] = obj.list[0]
         }
     }
 }
@@ -39,19 +39,19 @@ const
         }
     }
 
-const dataOed01 = jsLib1.makeElFrom(d, 'siteTitle count')
-dataOed01.init = d.init
-dataOed01.hashTitle = jsLib1.hashTitle
-dataOed01.openedAdnVlMenu = d.openedAdnVlMenu
-dataOed01.adnIdMO = d.adnIdMO
-dataOed01.eMap = d.eMap
-dataOed01.parentChild = d.parentChild
-dataOed01.leMap = Object.keys(dataOed01.eMap).length
+const d_Oed01 = jsLib1.makeElFrom(d, 'siteTitle count')
+d_Oed01.init = d.init
+d_Oed01.hashTitle = jsLib1.hashTitle
+d_Oed01.openedAdnVlMenu = d.openedAdnVlMenu
+d_Oed01.adnIdMO = d.adnIdMO
+d_Oed01.eMap = d.eMap
+d_Oed01.parentChild = d.parentChild
+d_Oed01.leMap = Object.keys(d_Oed01.eMap).length
 jsLib1.i = (adnId, n) => d.eMap[adnId] && d.eMap[adnId][n]
-export default dataOed01
+export default d_Oed01
 
 const oed01 = createApp({
-    data() { return dataOed01 }, methods: {
+    data() { return d_Oed01 }, methods: {
         adnMO(adnId) { this.adnIdMO = adnId }
         , o(adnId) { return d.eMap[adnId] }
         , i(n) { return jsLib1.i(this.adnId, n) }
@@ -65,7 +65,7 @@ oed01.component('t-oed01-value', {
                 d.init.tree[lr].openIds = d.init.tree[lr].openIds.filter(n => n !== adnId)
             else d.init.tree[lr].openIds.push(adnId)
         },
-    }, data() { return dataOed01 },
+    }, data() { return d_Oed01 },
     template: "#tOed01Value", props: { adnId: Number, lr: String },
 });
 oed01.component('t-oed01-oc', {//Oc: Open children
@@ -73,13 +73,13 @@ oed01.component('t-oed01-oc', {//Oc: Open children
         i(n, adnId) { return jsLib1.i(adnId || this.adnId, n) }
         , adnMO(adnId) { this.adnIdMO = adnId }
         , o(adnId) { return d.eMap[adnId] }
-    }, template: "#tOed01Oc", props: { parentId: Number, lr: String }, data() { return dataOed01 },
+    }, template: "#tOed01Oc", props: { parentId: Number, lr: String }, data() { return d_Oed01 },
 });
 oed01.component('t-oed01-mo', {
     methods: {
         i(n) { return jsLib1.i(this.adnId, n) }
         , closeAdnVlMenu() { this.openedAdnVlMenu = null }
-    }, template: "#tOed01Mo", props: { adnId: Number }, data() { return dataOed01 },
+    }, template: "#tOed01Mo", props: { adnId: Number }, data() { return d_Oed01 },
 });
 oed01.component('t-oed01-edv22', {
     methods: {
@@ -99,23 +99,22 @@ oed01.component('t-oed01-lmenu', {
         , edAdnValue01() { this.openedAdnVlMenu = this.adnId }
         , i(n) { return jsLib1.i(this.adnId, n) }
     }, template: "#tOed01Lmenu", props: { adnId: Number, edValue22: String }, data() {
-        return dataOed01
+        return d_Oed01
     },
 });
 oed01.mount('#oed01')
 
 oed01.config.errorHandler = err => console.error('-oed01-', err)
 
-
-createApp({ data() { return dataOed01 } }).mount('#headTitle')
+createApp({ data() { return d_Oed01 } }).mount('#headTitle')
 
 createApp({
     methods: {
         rld() {
             this.count++
-            if (Object.keys(dataOed01.eMap).length != dataOed01.leMap) {
-                console.log(Object.keys(dataOed01.eMap).length, dataOed01.leMap)
+            if (Object.keys(d_Oed01.eMap).length != d_Oed01.leMap) {
+                console.log(Object.keys(d_Oed01.eMap).length, d_Oed01.leMap)
             }
         }, treeStr() { return jsLib1.treeStr() },
-    }, data() { return dataOed01 },
+    }, data() { return d_Oed01 },
 }).mount('#headPage')
