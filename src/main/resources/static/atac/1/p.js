4209b2f001bc9e84991835e3592cc3aac23d7afc
romanm <roman.mishchenko@gmail.com>
2023-02-01 10:53:59 +0200

    work 𝑓 ed.v.0.0.1

--- src/main/resources/static/atac/1/p.js
+++ src/main/resources/static/atac/1/p.js
@@ -23,12 +23,16 @@ pd.cellValue = (r, c) => {
 }
 pd.tc.vRC = { 1: { 1: 1 }, 2: { 1: 2 }, 3: { 1: 3 }, }
 pd.dMap = { 1: { v: 4 }, 2: { v: 5 }, 3: { sum: [1, 2] } }
+pd.fnList = "_sum_max_cnt_avg_"
 pd.dMapMaxKey = () => Object.keys(pd.dMap).reduce((a, b) => Math.max(a, b)
     , -Infinity)
 pd.count = 0
 export default pd
 console.log(pd)
+//dev
 cdb.dMap = pd.dMap
+cdb.fnList = pd.fnList
+//dev::END
 
 pd.edCellAdress = window.location.hash.split('_')[1]
 pd.cellCoordinate = (c, r) => 'C' + c + 'R' + r
@@ -58,9 +62,11 @@ atac01.component('t-tac01-edcell', {
                 console.log(c)
             }
             pd.dMap[this.dmKey || c.k].v = 1 * e.target.value
+        }, cellObj() {
+            return pd.dMap[this.dmKey] || {}
         }, okSave() {
             console.log(pd.dMap)
-            pd.count++
+            // pd.count++
         }
     }, template: "#tTac01Edcell"
 })
