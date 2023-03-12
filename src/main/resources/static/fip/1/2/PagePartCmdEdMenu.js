96389524c3b02ff99f2bafcaa1c88b32a562edef
romanm <roman.mishchenko@gmail.com>
2023-03-12 15:17:21 +0200

    <input :value="fipi().json[pp].join(',')

--- src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
+++ src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
@@ -11,13 +11,16 @@ export default {
 <span class="w3-dropdown-click">
     <button @click="ppCmdEdOnOff"
         class="w3-btn w3-ripple w3-padding-small w3-small">&nbsp;&nbsp;
-        {ppMenuList.join(',')}}&nbsp;☰&nbsp;
+            {{fipi().pps.join(',')}}&nbsp;☰&nbsp;
         </button> <span class="w3-hide"> {{count}} </span>
     <div id="ppCmdEd" class="w3-dropdown-content w3-container w3-hover-shadow w3-border"
         style="right: -1em; width: 30em;">
         &nbsp;
         <div v-for="pp in fipi().pps" class="w3-hover-shadow">
-        {{pp}}:{{fip(pp)}}
+        <input :value="fipi().json[pp].join(',')" class="w3-hover-shadow">
+        <span class="w3-opacity w3-right am-u">
+            {{pp}}: <span class="w3-small am-i"> {{fip(pp)}} </span>
+        </span>
         </div>
         &nbsp;
     </div>
