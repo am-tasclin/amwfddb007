0b53a20f2f3c973927e74fd18afb7b735db03fa6
romanm <roman.mishchenko@gmail.com>
2023-03-15 00:44:10 +0200

    inputPanelPart

--- src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
+++ src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
@@ -5,6 +5,10 @@ export default {
     methods: {
         fipi() { return fipi }, fip(fip) { return wsDbC.fip[fip] },
         ppCmdEdOnOff() { pd.cmd.W3ShowOnOff('ppCmdEd') },
+        inputPanelPart(event) {
+            console.log(event.target)
+            console.log(event.target.value)
+        },
     },
     mounted() { pd.ppCmdEd = this }, data() { return { count: 1, } },
     template: `
@@ -14,13 +18,29 @@ export default {
             {{fipi().pps.join(',')}}&nbsp;☰&nbsp;
         </button> <span class="w3-hide"> {{count}} </span>
     <div id="ppCmdEd" class="w3-dropdown-content w3-container w3-hover-shadow w3-border"
-        style="right: -1em; width: 30em;">
+        style="right: -1em; width: 50em;">
+        <div class="w3-row w3-tiny am-b">
+            <div class="w3-half"> pageParts </div> <div class="w3-half"> pane2, right </div>
+        </div>
         &nbsp;
-        <div v-for="pp in fipi().pps" class="w3-hover-shadow">
-        <input :value="fipi().json[pp].join(',')" class="w3-hover-shadow">
-        <span class="w3-opacity w3-right am-u">
-            {{pp}}: <span class="w3-small am-i"> {{fip(pp)}} </span>
-        </span>
+        <div class="w3-row w3-border-bottom" v-for="pp in fipi().pps" class="w3-hover-shadow">
+            <div class="w3-half"> 
+                <span class="w3-opacity a1m-u">
+                    {{pp}}: <span class="w3-small am-i"> {{fip(pp)}} </span>
+                </span>
+                <input :value="fipi().json[pp].join(',')" class="w3-hover-shadow w3-small" style="width: 100%;">
+            </div>
+            <div class="w3-half w3-container w3-small">
+            <span v-for="k2 in fipi().json[pp]">
+                <template v-if="'lr'!=pp">
+                    <span class="w3-hover-shadow">
+                        <label> <input type="checkbox" :value="k2"
+                            @input="inputPanelPart">&nbsp;
+                        {{k2}}</label>
+                    </span>,
+                </template>
+            </span>
+            </div>
         </div>
         &nbsp;
     </div>
