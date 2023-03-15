da8fd41e56d05ab36be0b4fb54899d97e219b733
romanm <roman.mishchenko@gmail.com>
2023-03-15 13:38:53 +0200

    fipi: URI, JSON

--- src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
+++ src/main/resources/static/fip/1/2/PagePartCmdEdMenu.js
@@ -19,26 +19,35 @@ export default {
         </button> <span class="w3-hide"> {{count}} </span>
     <div id="ppCmdEd" class="w3-dropdown-content w3-container w3-hover-shadow w3-border"
         style="right: -1em; width: 50em;">
-        <div class="w3-row w3-tiny am-b">
-            <div class="w3-half"> pageParts </div> <div class="w3-half"> pane2, right </div>
-        </div>
-        <div class="w3-row w3-border-bottom" v-for="pp in fipi().pps" class="w3-hover-shadow">
-            <div class="w3-half"> 
-                <span class="w3-opacity a1m-u">
-                    {{pp}}: <span class="w3-small am-i"> {{fip(pp)}} </span>
-                </span>
-                <input :value="fipi().json[pp].join(',')" class="w3-hover-shadow w3-small" style="width: 100%;">
+        
+        <div class="w3-row">
+            <div class="w3-quarter">
+                <div class="w3-tiny am-b w3-border-bottom">URI, JSON</div>
+                a1
             </div>
-            <div class="w3-half w3-container w3-small">
-            <span v-for="k2 in fipi().json[pp]">
-                <template v-if="'lr'!=pp">
-                    <span class="w3-hover-shadow">
-                        <label> <input type="checkbox" :value="k2"
-                            @input="inputPanelPart">&nbsp;
-                        {{k2}}</label>
-                    </span>,
-                </template>
-            </span>
+            <div class="w3-threequarter">
+                <div class="w3-row w3-tiny am-b w3-border-bottom">
+                    <div class="w3-half"> pageParts </div> <div class="w3-half"> pane2, right </div>
+                </div>
+                <div class="w3-row w3-border-bottom" v-for="pp in fipi().pps" class="w3-hover-shadow">
+                    <div class="w3-half"> 
+                        <span class="w3-opacity a1m-u">
+                            {{pp}}: <span class="w3-small am-i"> {{fip(pp)}} </span>
+                        </span>
+                        <input :value="fipi().json[pp].join(',')" class="w3-hover-shadow w3-small" style="width: 100%;">
+                    </div>
+                    <div class="w3-half w3-container w3-small">
+                    <span v-for="k2 in fipi().json[pp]">
+                        <template v-if="'lr'!=pp">
+                            <span class="w3-hover-shadow">
+                                <label> <input type="checkbox" :value="k2"
+                                    @input="inputPanelPart">&nbsp;
+                                {{k2}}</label>
+                            </span>,
+                        </template>
+                    </span>
+                    </div>
+                </div>
             </div>
         </div>
         &nbsp;
