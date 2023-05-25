3b1f377fec3e441d4fbf85b08adff9bd8511565d
romanm <roman.mishchenko@gmail.com>
2023-05-25 20:44:42 +0300

    TFF

--- src/main/resources/static/f/3/ff/TFF.js
+++ src/main/resources/static/f/3/ff/TFF.js
@@ -5,14 +5,31 @@ export default {
     data() { return { count: 0 } },
     mounted() {
         dbMpView.ff = this
+        console.log(dbMpView)
     },
     methods: {
-        ffl() { return dbMpData.list }
+        ffl() { return dbMpData.list },
+        ffh() { return dbMpData.list && Object.keys(dbMpData.list[0]) },
     },
     template: `
-a1 FF
-{{ffl()}}
-a2
-{{count}}
+    ff2
+<table>
+    <thead>
+        <tr class="w3-tiny am-b">
+            <th class="w3-light-gray" v-for="ch in ffh()">
+                {{ch}}
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="r in ffl()" class="w3-hover-shadow">
+            <td class="w3-small">
+                <a :href="'/f/3/twiki/i.html#page,'+r.string_id">{{r.string_id}}</a>
+            </td>
+            <td>{{r.value_22}}</td>
+        </tr>
+    </tbody>
+</table><snap class="w3-hide">{{count}}</snap>
+<p> &nbsp; </p>
 `,
 }
