40ad1c88558813cda164b169c4b0ff6ddf98ece0
romanm <roman.mishchenko@gmail.com>
2023-06-02 01:13:05 +0300

    adnMenu

--- src/main/resources/static/f/4/lib/AdnMenu.js
+++ src/main/resources/static/f/4/lib/AdnMenu.js
@@ -4,9 +4,21 @@ export default {
     methods: {
         adnClick() {
             console.log(this.adnId)
+        }, setAdnDialogWindow(type, editType) {
+            console.log('123', type, editType)
         }, sortUp() {
             console.log('fipiFn.sortUpDown(-1, this.adnId)')
         }, sortDown() {
+            console.log('123')
+        }, deleteAdn() {
+            console.log('123')
+        }, insertAdnChild() {
+            console.log('123')
+        }, cleanEdit() {
+            console.log('123')
+        }, setCopy() {
+            console.log('123')
+        }, setCut() {
             console.log('123')
         }
     },
@@ -18,10 +30,22 @@ export default {
         <div class="w3-tiny"> &nbsp; r: r2: 
             <span class="w3-right"> p: 123 </span>
         </div>
-        <button class="w3-btn" @click="sortUp()">⬆</button>
-        <button class="w3-btn" @click="sortDown()">⬇</button>
+        <button class="w3-btn" @click="sortUp">⬆</button>
+        <button class="w3-btn" @click="sortDown">⬇</button>
         ｜
+        <button class="w3-btn am-b" @click="insertAdnChild" title="addChild - додати дитину">＋</button>
+        <button class="w3-btn am-b" @click="deleteAdn">－</button>
+        <div class="w3-border-top">
+            <button class="w3-btn am-b" @click="setAdnDialogWindow('edit','fixed')">✐</button>
+            <button class="w3-btn am-b" @click="setAdnDialogWindow('edit','fly')">✎</button>
+            ｜
+            <button class="w3-btn am-b w3-text-blue" @click="cleanEdit">－✎⧉</button>
+        </div>
+        <div class="w3-border-top">
+            <button class="w3-btn am-b" @click="setCopy" title="copy - копіювати">⧉</button>
+            <button class="w3-btn am-b" @click="setCut" title="cut - вирізати">✀</button>
+        </div>
     </div>
 </span>
-    `,
+`,
 }
