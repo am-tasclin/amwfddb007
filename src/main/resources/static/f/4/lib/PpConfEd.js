d9b08bb1bcc47d7ba1c582bb1491816aca10a582
romanm <roman.mishchenko@gmail.com>
2023-06-08 02:23:35 +0300

    ppCmd,ppConf transfer from PagePartCmdEdMenu OK

--- src/main/resources/static/f/4/lib/PpConfEd.js
+++ src/main/resources/static/f/4/lib/PpConfEd.js
@@ -1,6 +1,5 @@
 'use strict'
-import { confPP } from '/f/4/lib/metal.js'
-import { ppInteractivity } from '/f/4/lib/metal.js'
+import { confPP, ppInteractivity } from '/f/4/lib/metal.js'
 import MCDataSort from '/f/4/lib/MCDataSort.js'
 
 export default {
@@ -11,13 +10,16 @@ export default {
         ppInteractivity.fn.ppId(this.ppId)[ppConfEdKey] = this
 
     }, methods: {
+        confPP() { return confPP.ppId[this.ppId || 1] },
         confTypeName(showMedasConfTypeName) {
             this.medasConfTypeName = ppInteractivity.medasConfTypeName = showMedasConfTypeName
         }, confJsonStr() {
             return JSON.stringify(confPP, '', 2)
                 .replace(/\s+]/g, ']')
                 .replace(/\s+}/g, '}')
-        },
+        },medasMcdId(event, ppId, medas) {
+            console.log(event.target.value.split(','), ppId, medas)
+        }
     }, template: `
 <div class="w3-container">
     <div class="w3-row">
@@ -37,9 +39,51 @@ export default {
                 {{confJsonStr()}}
                 <div>&nbsp;</div>
             </div>
+            <template v-else>
+                <div v-for="medas in confPP().l_medas" class="w3-opacity w3-tiny"
+                        style="white-space: pre; overflow: auto;">
+                    <b> {{medas}}</b>,
+                    {{confPP().medas[medas].l_mcdId.join(',')}}
+                    <div v-if="confPP().medas[medas].ppl2">
+                        <b>{{medas}}_ppl2</b>,
+                        {{confPP().medas[medas].ppl2.l_mcdId.join(',')}}
+                    </div>
+                </div>
+            </template>
         </div>
         <div class="w3-threequarter w3-container">
-            Hi a123
+            <div class="w3-row w3-tiny am-b w3-border-bottom">
+                <div class="w3-half"> pageParts </div>
+                <div class="w3-half w3-container"> pane2, right </div>
+            </div>
+            <div v-for="medas in confPP().l_medas" class="w3-row w3-border-bottom w3-hover-shadow">
+                <div class="w3-half">
+                    <span class="w3-opacity a1m-u">{{medas}}</span>
+                    <input @keyup.enter="medasMcdId($event, ppId, medas)" 
+                        :value="confPP().medas[medas].l_mcdId.join(', ')"
+                        class="w3-hover-shadow w3-small am-width-100pr">
+                    <div class="w3-tiny">
+                        <MCDataSort :ppId="ppId" :medas="medas" location="ppCmd"/>
+                    </div>
+                </div>
+                <div class="w3-half w3-container">
+                    &nbsp;
+                    <input @keyup.enter="medasMcdId($event, ppId, medas, true)" 
+                        :value="confPP().medas[medas].ppl2 && confPP().medas[medas].ppl2.l_mcdId.join(', ')"
+                        v-if="'lr'==medas"
+                        class="w3-hover-shadow w3-small am-width-100pr">
+                        <div v-if="confPP().medas[medas].ppl2" class="w3-tiny">
+                            <MCDataSort :ppId="ppId" :medas="medas" 
+                                :location="medas+'_ppl2'"/>
+                        </div>
+                    <template v-if="'lr'!=medas">
+                        <template v-for="mcdId in confPP().medas[medas].l_mcdId">
+                            <label><input v-model="epl2Data[medas]"
+                                type="checkbox" :value="mcdId"/>&nbsp;{{mcdId}}</label>,
+                        </template>
+                    </template>
+                </div>
+            </div>
         </div>
     </div>
 </div> <span class="w3-hide">{{count}}</span>
