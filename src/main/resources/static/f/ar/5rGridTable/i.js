f0ddfe9ad9755d9f9ee0be5fbace6249242640ad
romanm <roman.mishchenko@gmail.com>
2023-07-29 15:57:24 +0300

    initSelectMaker

---
+++ src/main/resources/static/f/ar/5rGridTable/i.js
@@ -0,0 +1,32 @@
+'use strict'
+const { createApp } = Vue
+import { makeGridTable } from './libGridTable/libGridTable.js'
+
+const gridKassaEntry = makeGridTable('kassaEntry')
+    , kassaEntry01SelectMaker = gridKassaEntry.initSelectMaker('ksEy01', 'kassa.entry')
+const selectKassaEntry = kassaEntry01SelectMaker.get()
+console.log(selectKassaEntry)
+console.log(kassaEntry01SelectMaker.initColumns(' count(*) AS cnt ').get())
+
+import { ws, executeSelectQuery } from '/f/6/lib/wsDbRw.js'
+ws.onopen = event =>
+    executeSelectQuery(selectKassaEntry
+    ).then(json => {
+        console.log('лінія 17 і так написано', json)
+        gridKassaEntry.setTableBody(json.list)
+        gridKassaEntry.get().tBody.count++
+    })
+
+
+import TBody from './libGridTable/TBody.js'
+createApp({
+    components: { TBody },
+    template: `
+<div style="max-height:22em; overflow: auto;">
+    <table class="am-hf-sticky01">
+        <caption>Hi table! 5:t⇐dd</caption>
+        <TBody gridTableName="kassaEntry" />
+    </table>&nbsp;
+</div>&nbsp;
+`,
+}).mount('#tGridTable1')
---
+++ src/main/resources/static/f/ar/5rGridTable/libGridTable/libGridTable.js
@@ -0,0 +1,68 @@
+'use struct'
+/**
+ * Algoritmed ©, Licence EUPL-1.2 or later.
+ * 
+ */
+
+const gridTableContainer = {}
+/**
+ * key:value -- all structuren for one or more accounting tables
+ */
+
+/**
+ * Get or set and get one accounting table by key
+ * Отримати або встановити та отримати одну облікову таблицю за ключем
+ * @param {*} tableName 
+ * @returns 
+ */
+export const gridTable = tableName => gridTableContainer[tableName]
+    || (gridTableContainer[tableName] = {})
+
+/**
+ * Function block for build and use accounting table
+ * Функціональний блок для створення та використання облікової таблиці
+ * @param {*} tableName 
+ * @returns 
+ */
+export const makeGridTable = tableName => {
+    const mGridTable = gridTable(tableName)
+    return {
+        get() { return mGridTable },
+        setTableBody(tableBody) {
+            mGridTable.tableBody = tableBody
+            !mGridTable.bodyColumns && this.setBodyColumns(Object.keys(tableBody[0]))
+            console.log(gridTableContainer)
+        },
+        setBodyColumns(bodyColumns) { mGridTable.bodyColumns = bodyColumns },
+        initSelectMaker(key, sqlTableName) {
+            const selectMakerContainer = mGridTable.smc || (mGridTable.smc = {})
+                , makeSelectForKey = selectMakerContainer[key] || (selectMakerContainer[key] = {})
+            return SqlSelectMaker(makeSelectForKey, sqlTableName)
+        },
+        showGtData(){return mGridTable}
+    }
+}
+
+/**
+ * Block of functions to generate the SQL select with it init data collection
+ * Блок функцій для генерації SQL select з його ініціальним набором даних
+ * @param {*} smContainer 
+ * @param {*} sqlTableName 
+ * @returns 
+ */
+const SqlSelectMaker = (smContainer, sqlTableName) => {
+    return {
+        sqlTableName: sqlTableName,
+        setFrom(from) { smContainer.from = from },
+        getFrom() { return smContainer.from || sqlTableName },
+        initColumns(columns) {
+            smContainer.columns = columns
+            return this
+        },
+        getColumns() { return smContainer.columns || ' * ' },
+        get() {
+            return 'SELECT '.concat(this.getColumns())
+                .concat(' FROM ').concat(this.getFrom())
+        },
+    }
+}
